c20eaa55a46a889044ec248b4711279eaa788320
SolanoD94 <dianasolano4970@gmail.com>
2023-05-23 23:45:15 -0600

    debugged server and installed apollo-client in client side

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -43,7 +43,7 @@ const typeDefs = gql`
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(book: BookInput!): User
-    ls
+    removeBook(bookId: ID!): User
   }
 `;
 
--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
 const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
+const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
-const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
+const { typeDefs, resolvers } = require("./schemas");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  introspection: true,
   context: authMiddleware,
 });
 
